9b2a36dc0b8f08a52075c80e2f413e63c7c6f730
sergemaunda <serge.maunda.3@gmail.com>
2024-02-17 19:34:03 +0200

    Added camera plugin for scanning items

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -3,8 +3,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { Tab1PageRoutingModule } from './tab1-routing.module';
 
 @NgModule({
@@ -12,9 +11,9 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
     IonicModule,
     CommonModule,
     FormsModule,
-    ExploreContainerComponentModule,
     Tab1PageRoutingModule
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   declarations: [Tab1Page]
 })
 export class Tab1PageModule {}
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PhotoService } from '../services/photo.service';
 
 @Component({
   selector: 'app-tabs',
@@ -7,6 +8,11 @@ import { Component } from '@angular/core';
 })
 export class TabsPage {
 
-  constructor() {}
+  constructor(private photoService: PhotoService) {}
+
+  scanItem() {
+    this.photoService.scanNewItem();
+  }
+
 
 }
